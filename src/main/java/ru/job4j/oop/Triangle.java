0a3908eb6edbe67a8752a5ea30093a0a789f346c
Grossevich Lev <levgross@gmail.com>
2022-01-22 14:38:48 +0400

    убрал лишние присвоения переменным в методах semiPerimeter  и exist

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -14,16 +14,10 @@ public class Triangle {
     }
 
     public double semiPerimeter(double a, double b, double c) {
-        a = first.distance(second);
-        b = first.distance(third);
-        c = second.distance(third);
         return (a + b + c) / 2;
     }
 
     public boolean exist(double ab, double ac, double bc) {
-        ab = first.distance(second);
-        ac = first.distance(third);
-        bc = second.distance(third);
         return ab + ac > bc && ab + bc > ac && ac + bc > ab;
     }
 
