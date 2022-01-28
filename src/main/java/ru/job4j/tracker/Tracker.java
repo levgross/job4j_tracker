9040102dee9e68653c454dbb089b3fcdd57f32df
Grossevich Lev <levgross@gmail.com>
2022-01-28 10:57:41 +0400

    упростил метод findAll

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,17 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int num = 0;
-        for (int i = 0; i < size; i++) {
-            Item item = items[i];
-            if (item != null) {
-                rsl[num] = item;
-                num++;
-            }
-        }
-        rsl = Arrays.copyOf(rsl, num);
-        return rsl;
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
