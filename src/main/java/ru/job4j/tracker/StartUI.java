8c984cd2a68ecb19179c07f138a00c2c1f03f78a
Grossevich Lev <levgross@gmail.com>
2022-01-30 19:23:03 +0400

    Переименовал классы

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -23,8 +23,8 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                        new CreateAction(), new ShowAllAction(), new EditAction(), new DeleteAction(),
-                        new FindByIdAction(), new FindItemsByNameAction(), new ExitAction()
+                        new CreateAction(), new ShowAllAction(), new ReplaceAction(), new DeleteAction(),
+                        new FindByIdAction(), new FindItemsByNameAction(), new Exit()
         };
         new StartUI().init(input, tracker, actions);
     }
