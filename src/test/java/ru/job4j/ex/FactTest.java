22e1d1776a4525fa319bf0dfe7c6a37327f7249c
Grossevich Lev <levgross@gmail.com>
2022-01-31 14:18:53 +0400

    0.3. Тестирование исключений с junit [#219366]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.*;
 public class FactTest {
     @Test
     public void whenNIs5Then120() {
-        int rsl = Fact.calc(5);
+        int rsl = new Fact().calc(5);
         assertThat(rsl, is(120));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenNIsLess0ThenFinish() {
-        int rsl = Fact.calc( -1);
+        int rsl = new Fact().calc( -1);
     }
 }
