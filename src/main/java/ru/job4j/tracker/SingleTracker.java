08fb4f01fd63f8724e4d4f6630056cfa329f08ac
Grossevich Lev <levgross@gmail.com>
2022-02-02 12:53:10 +0400

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public final class SingleTracker {
+    private static Tracker tracker = null;
+
+    private SingleTracker() {
+    }
+
+    public static Tracker getTracker() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
