38cbd3e955ac42cb874bac015aa98ed6168afcb7
Grossevich Lev <levgross@gmail.com>
2022-05-06 16:06:55 +0400

    3. Liquibase. Интеграционные тесты. [#95881 #297988]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
@@ -60,5 +61,48 @@ public class SqlTrackerTest {
         Item item = new Item("item");
         tracker.add(item);
         assertTrue(tracker.delete(item.getId()));
+    }
+
+    @Test
+    public void whenReplaceItemByGeneratedIdThenTrue() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        Item newItem = new Item("newItem");
+        tracker.add(item);
+        assertTrue(tracker.replace(item.getId(), newItem));
+        assertThat(tracker.findById(item.getId()).getName(), is("newItem"));
+    }
+
+    @Test
+    public void whenFindByNameThen2Items() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        Item item1 = new Item("item");
+        Item item2 = new Item("item2");
+        tracker.add(item);
+        tracker.add(item1);
+        tracker.add(item2);
+        List<Item> items = List.of(
+                tracker.findById(item.getId()),
+                tracker.findById(item1.getId())
+        );
+        assertEquals(items, tracker.findByName("item"));
+    }
+
+    @Test
+    public void whenFindAllThen3Items() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        Item item1 = new Item("item");
+        Item item2 = new Item("item2");
+        tracker.add(item);
+        tracker.add(item1);
+        tracker.add(item2);
+        List<Item> items = List.of(
+                tracker.findById(item.getId()),
+                tracker.findById(item1.getId()),
+                tracker.findById(item2.getId())
+        );
+        assertEquals(items, tracker.findAll());
     }
 }
