2d5eac488eacb59a413c3e76c46c4f759824e74a
Grossevich Lev <levgross@gmail.com>
2022-10-28 14:37:38 +0400

    2. @Setter, @Getter и другие

---
+++ src/main/java/ru/job4j/lombok/Category.java
@@ -0,0 +1,14 @@
+package ru.job4j.lombok;
+
+import lombok.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
+public class Category {
+    @NonNull
+    @EqualsAndHashCode.Include
+    @Getter
+    private int id;
+    @Setter@Getter
+    private String name;
+}
