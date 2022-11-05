b945641af57e6b0b1c18c51ec46a61f84c1e34c3
Grossevich Lev <levgross@gmail.com>
2022-11-05 19:37:00 +0400

    To Many

---
+++ db/scripts/update_005.sql
@@ -0,0 +1,6 @@
+create table if not exists j_user_notification (
+    id serial primary key,
+    messenger text,
+    identify text,
+    j_user_id int REFERENCES j_user(id)
+);
--- src/main/java/ru/job4j/toone/User.java
+++ src/main/java/ru/job4j/toone/User.java
@@ -1,9 +1,10 @@
 package ru.job4j.toone;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
+import lombok.EqualsAndHashCode.Include;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -12,10 +13,13 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
+    @Include
     private int id;
     private String name;
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "j_user_id")
+    private List<UserMessenger> messengers;
 }
